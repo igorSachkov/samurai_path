b0b567b271243866020fa12af9cabeee1ecb0066
igorSachkov <igordadadadanet@yandex.ru>
2021-08-04 22:58:06 +0300

    footer темы, material гамбургер

--- src/Content/Header/Header.js
+++ src/Content/Header/Header.js
@@ -2,16 +2,41 @@ import Logo from "./Logo/Logo"
 import MenuArea from "./MenuArea/MenuArea"
 import style1 from "./Header.module.css"
 import style2 from "./../Common/ThemeStyle.module.css"
-const Header = (props)=> {
+import { IconButton } from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+const Header = (props) => {
     const stylesMap = {
         style1,
         style2
     }
+    const classes = useStyles();
     return (
         <div className={`${stylesMap.style1.container} ${stylesMap.style2[props.theme]}`}>
             <div className={style1.header__wrapper}>
                 <Logo></Logo>
-                <MenuArea {...props}></MenuArea>
+                <div className={style1.login}>
+                    <MenuArea {...props}></MenuArea>
+                </div>
+
+                <div className={style1.hamburger}>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                </div>
+
             </div>
         </div>
     )
--- src/Content/MobileFooter/MobileFooter.js
+++ src/Content/MobileFooter/MobileFooter.js
@@ -1,13 +1,21 @@
-import c from "./MobileFooter.module.css"
+import style1 from "./MobileFooter.module.css"
+import style2 from "./../Common/ThemeStyle.module.css"
 import MusicIcon from "../Images/music-player.png"
 import Dialogs from "../Images/dialog.png"
 import profileIcon from "../Images/profile-icon.png"
 import news from "../Images/news.png"
 import { NavLink } from "react-router-dom"
+import { connect } from "react-redux"
 
-const MobileFooter = function () {
+
+
+const MobileFooter =  (props) => {
+    const stylesMap = {
+        style1,
+        style2
+    }
     return (
-        <div className={c.mobileFooterContainer}>
+        <div className={`${stylesMap.style1.mobileFooterContainer} ${stylesMap.style2[props.theme]}`}>
             <NavLink to={`profile`}>
             <img src={profileIcon}></img>
             </NavLink>
@@ -23,5 +31,9 @@ const MobileFooter = function () {
         </div>
     )
 }
-
-export default MobileFooter;
+const mapStateToProps = (state) => {
+    return {
+        theme: state.settings.theme
+    }
+}
+export default connect(mapStateToProps, null)(MobileFooter);
